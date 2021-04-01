c03c184de91b93117cc966da0b70a84120eba76d
ericbenmax <ericbenmax@yahoo.com.tw>
2021-04-01 11:13:48 +0800

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by 陳士偉 on 2021/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
